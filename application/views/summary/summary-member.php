8a543b6779fc58c0512cbae8f262057ffa0af512
icyflame <kannan.siddharth12@gmail.com>
2014-07-27 23:21:41 +0530

    [minor] Removed on line break

--- application/views/summary/summary-member.php
+++ application/views/summary/summary-member.php
@@ -44,7 +44,7 @@
 	echo 'Logged in.'.'<br/>'.'<br/>';
 
 	echo 'Hey, '.$this->session->userdata('username').'.<br/>';
-	echo 'You have a privilege of '.$this->session->userdata('privilege').'<br/>'.'<br/>';
+	echo 'You have a privilege of '.$this->session->userdata('privilege').'<br/>';
 
 	// echo print_r($this->session->all_userdata());
 
