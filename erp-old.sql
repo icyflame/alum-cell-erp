0aec03ec0773faccb4905666c48d0220f2c3d8f2
icyflame <kannan.siddharth12@gmail.com>
2014-07-28 14:34:58 +0530

    [add-table] Added the calling table. And a python script to populate it.
    And a populated sql file.

--- erp-old.sql
+++ erp-old.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jul 28, 2014 at 11:03 AM
+-- Generation Time: Jul 24, 2014 at 04:59 AM
 -- Server version: 5.5.36
 -- PHP Version: 5.4.25
 
@@ -19,8 +19,10 @@ SET time_zone = "+00:00";
 --
 -- Database: `erp`
 --
+
 CREATE DATABASE IF NOT EXISTS `erp` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `erp`;
+
 -- --------------------------------------------------------
 
 --
@@ -40,54 +42,54 @@ CREATE TABLE IF NOT EXISTS `alumni` (
 -- Dumping data for table `alumni`
 --
 
-INSERT INTO `alumni` (`alumid`, `name`, `hall`, `dept`, `alumSince`) VALUES
-(1, 'John Doe', 'CS', 'Rajendra Prasad Hall', 1965),
-(2, 'Atanu Das', 'CS', 'Rajendra Prasad Hall', 1965),
-(3, 'B K Tariyal', 'CS', '', 1965),
-(4, 'Jyoti Parshad Banerjee', 'CS', '', 1965),
-(5, 'K K Saxena', 'CS', 'Rajendra Prasad Hall', 1965),
-(6, 'Kiran Sankar Sahu', 'CS', '', 1965),
-(7, 'Mithilesh Kumar Sinha', 'CS', 'Azad Hall', 1965),
-(8, 'Satish Bansal', 'CS', 'Nehru Hall', 1965),
-(9, 'Tapash Kumar Pal', 'CS', 'Rajendra Prasad Hall', 1965),
-(10, 'Amitava De', 'CS', 'Lala Lajpat Rai Hall', 1975),
-(11, 'Ashok Kumar Ghosh', 'CS', 'Lala Lajpat Rai Hall', 1975),
-(12, 'Niladri Nirjhar Biswas', 'CS', '', 1975),
-(13, 'Partha Sen', 'ME', 'Rajendra Prasad Hall', 1975),
-(14, 'Pramod Kumar Gupta', 'ME', 'Vidya Sagar Hall', 1975),
-(15, 'Shyam Sunder Verma', 'ME', 'Vidya Sagar Hall', 1975),
-(16, 'Sudip Sen', 'ME', 'Nehru Hall', 1975),
-(17, 'Sushim Kumar Dasgupta', 'ME', 'Patel Hall', 1975),
-(18, 'Tirthankar Banerjee', 'ME', 'Patel Hall', 1975),
-(19, 'Swapan Guha', 'ME', 'Rajendra Prasad Hall', 1975),
-(20, 'Vinod Rai Juthani', 'ME', '', 1975),
-(21, 'S Anand', 'ME', 'Azad Hall', 1990),
-(22, 'Ashis Kumar Roy', 'ME', 'Azad Hall', 1990),
-(23, 'Dinesh Shastri', 'ME', 'Patel Hall', 1990),
-(24, 'J Ramesh', 'ME', 'Radhakrishnan Hall', 1990),
-(25, 'Jayanta Kumar Rudra', 'ME', '', 1990),
-(26, 'Peshwa Acharya', 'ME', 'Rajendra Prasad Hall', 1990),
-(27, 'Pradeep Kumar Bhalla', 'ME', 'Lala Lajpat Rai Hall', 1990),
-(28, 'Rajeev Kumar Saraf', 'CH', 'Nehru Hall', 1990),
-(29, 'Rakesh Chandubhai Pandya', 'CH', 'Rajendra Prasad Hall', 1990),
-(30, 'Ramakrishna Manne', 'CH', 'Azad Hall', 1990),
-(31, 'Ravindra Kumar Sinha', 'CH', 'Vidya Sagar Hall', 1990),
-(32, 'Sanjay Prakash Gupta', 'CH', 'Rajendra Prasad Hall', 1990),
-(33, 'Sanjay Dutt', 'CH', 'Lala Lajpat Rai Hall', 1990),
-(34, 'Sanjiv Kumar Singh', 'CH', 'Rajendra Prasad Hall', 1990),
-(35, 'Shambhu Sharan', 'CH', '', 1990),
-(36, 'Sibasish Padhi', 'CH', 'Vidya Sagar Hall', 1990),
-(37, 'Sujit Bhattacharyya', 'CH', 'Nehru Hall', 1990),
-(38, 'Sumit Das', 'CH', 'Lala Lajpat Rai Hall', 1990),
-(39, 'Sumit Chakraborty', 'CH', 'Radhakrishnan Hall', 1990),
-(40, 'Suresh Adina', 'CH', 'Azad Hall', 1990),
-(41, 'Tapash Kumar Gupta', 'CH', 'Patel Hall', 1975),
-(42, 'Chiranjit Ghosh', 'CH', 'Patel Hall', 1975),
-(43, 'L Ravindra Rao', 'CH', 'Patel Hall', 1975),
-(44, 'Samir Biswas', 'CH', '', 1975),
-(45, 'Kunal Bhattacharya', 'MF', 'Patel Hall', 1975),
-(46, 'Ranjit Kumar Jana', 'MF', 'Patel Hall', 1975),
-(47, 'Chandra Sekhar Bandyopadhyay', 'MF', 'Patel Hall', 1975);
+INSERT INTO `alumni` (`alumid`, `name`, `dept`, `hall`, `alumSince`) VALUES
+(1, 'John Doe', 'Rajendra Prasad Hall', 'CS', 1965),
+(2, 'Atanu Das', 'Rajendra Prasad Hall', 'CS', 1965),
+(3, 'B K Tariyal', '', 'CS', 1965),
+(4, 'Jyoti Parshad Banerjee', '', 'CS', 1965),
+(5, 'K K Saxena', 'Rajendra Prasad Hall', 'CS', 1965),
+(6, 'Kiran Sankar Sahu', '', 'CS', 1965),
+(7, 'Mithilesh Kumar Sinha', 'Azad Hall', 'CS', 1965),
+(8, 'Satish Bansal', 'Nehru Hall', 'CS', 1965),
+(9, 'Tapash Kumar Pal', 'Rajendra Prasad Hall', 'CS', 1965),
+(10, 'Amitava De', 'Lala Lajpat Rai Hall', 'CS', 1975),
+(11, 'Ashok Kumar Ghosh', 'Lala Lajpat Rai Hall', 'CS', 1975),
+(12, 'Niladri Nirjhar Biswas', '', 'CS', 1975),
+(13, 'Partha Sen', 'Rajendra Prasad Hall', 'ME', 1975),
+(14, 'Pramod Kumar Gupta', 'Vidya Sagar Hall', 'ME', 1975),
+(15, 'Shyam Sunder Verma', 'Vidya Sagar Hall', 'ME', 1975),
+(16, 'Sudip Sen', 'Nehru Hall', 'ME', 1975),
+(17, 'Sushim Kumar Dasgupta', 'Patel Hall', 'ME', 1975),
+(18, 'Tirthankar Banerjee', 'Patel Hall', 'ME', 1975),
+(19, 'Swapan Guha', 'Rajendra Prasad Hall', 'ME', 1975),
+(20, 'Vinod Rai Juthani', '', 'ME', 1975),
+(21, 'S Anand', 'Azad Hall', 'ME', 1990),
+(22, 'Ashis Kumar Roy', 'Azad Hall', 'ME', 1990),
+(23, 'Dinesh Shastri', 'Patel Hall', 'ME', 1990),
+(24, 'J Ramesh', 'Radhakrishnan Hall', 'ME', 1990),
+(25, 'Jayanta Kumar Rudra', '', 'ME', 1990),
+(26, 'Peshwa Acharya', 'Rajendra Prasad Hall', 'ME', 1990),
+(27, 'Pradeep Kumar Bhalla', 'Lala Lajpat Rai Hall', 'ME', 1990),
+(28, 'Rajeev Kumar Saraf', 'Nehru Hall', 'CH', 1990),
+(29, 'Rakesh Chandubhai Pandya', 'Rajendra Prasad Hall', 'CH', 1990),
+(30, 'Ramakrishna Manne', 'Azad Hall', 'CH', 1990),
+(31, 'Ravindra Kumar Sinha', 'Vidya Sagar Hall', 'CH', 1990),
+(32, 'Sanjay Prakash Gupta', 'Rajendra Prasad Hall', 'CH', 1990),
+(33, 'Sanjay Dutt', 'Lala Lajpat Rai Hall', 'CH', 1990),
+(34, 'Sanjiv Kumar Singh', 'Rajendra Prasad Hall', 'CH', 1990),
+(35, 'Shambhu Sharan', '', 'CH', 1990),
+(36, 'Sibasish Padhi', 'Vidya Sagar Hall', 'CH', 1990),
+(37, 'Sujit Bhattacharyya', 'Nehru Hall', 'CH', 1990),
+(38, 'Sumit Das', 'Lala Lajpat Rai Hall', 'CH', 1990),
+(39, 'Sumit Chakraborty', 'Radhakrishnan Hall', 'CH', 1990),
+(40, 'Suresh Adina', 'Azad Hall', 'CH', 1990),
+(41, 'Tapash Kumar Gupta', 'Patel Hall', 'CH', 1975),
+(42, 'Chiranjit Ghosh', 'Patel Hall', 'CH', 1975),
+(43, 'L Ravindra Rao', 'Patel Hall', 'CH', 1975),
+(44, 'Samir Biswas', '', 'CH', 1975),
+(45, 'Kunal Bhattacharya', 'Patel Hall', 'MF', 1975),
+(46, 'Ranjit Kumar Jana', 'Patel Hall', 'MF', 1975),
+(47, 'Chandra Sekhar Bandyopadhyay', 'Patel Hall', 'MF', 1975);
 
 -- --------------------------------------------------------
 
@@ -101,71 +103,6 @@ CREATE TABLE IF NOT EXISTS `assign` (
   `year` int(11) NOT NULL,
   PRIMARY KEY (`alumid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `calling`
---
-
-CREATE TABLE IF NOT EXISTS `calling` (
-  `alumid` int(11) NOT NULL,
-  `followup` date NOT NULL DEFAULT '2014-07-20',
-  `lastdate` date NOT NULL DEFAULT '2014-07-01'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `calling`
---
-
-INSERT INTO `calling` (`alumid`, `followup`, `lastdate`) VALUES
-(1, '2014-07-20', '2014-07-01'),
-(2, '2014-07-20', '2014-07-01'),
-(3, '2014-07-20', '2014-07-01'),
-(4, '2014-07-20', '2014-07-01'),
-(5, '2014-07-20', '2014-07-01'),
-(6, '2014-07-20', '2014-07-01'),
-(7, '2014-07-20', '2014-07-01'),
-(8, '2014-07-20', '2014-07-01'),
-(9, '2014-07-20', '2014-07-01'),
-(10, '2014-07-20', '2014-07-01'),
-(11, '2014-07-20', '2014-07-01'),
-(12, '2014-07-20', '2014-07-01'),
-(13, '2014-07-20', '2014-07-01'),
-(14, '2014-07-20', '2014-07-01'),
-(15, '2014-07-20', '2014-07-01'),
-(16, '2014-07-20', '2014-07-01'),
-(17, '2014-07-20', '2014-07-01'),
-(18, '2014-07-20', '2014-07-01'),
-(19, '2014-07-20', '2014-07-01'),
-(20, '2014-07-20', '2014-07-01'),
-(21, '2014-07-20', '2014-07-01'),
-(22, '2014-07-20', '2014-07-01'),
-(23, '2014-07-20', '2014-07-01'),
-(24, '2014-07-20', '2014-07-01'),
-(25, '2014-07-20', '2014-07-01'),
-(26, '2014-07-20', '2014-07-01'),
-(27, '2014-07-20', '2014-07-01'),
-(28, '2014-07-20', '2014-07-01'),
-(29, '2014-07-20', '2014-07-01'),
-(30, '2014-07-20', '2014-07-01'),
-(31, '2014-07-20', '2014-07-01'),
-(32, '2014-07-20', '2014-07-01'),
-(33, '2014-07-20', '2014-07-01'),
-(34, '2014-07-20', '2014-07-01'),
-(35, '2014-07-20', '2014-07-01'),
-(36, '2014-07-20', '2014-07-01'),
-(37, '2014-07-20', '2014-07-01'),
-(38, '2014-07-20', '2014-07-01'),
-(39, '2014-07-20', '2014-07-01'),
-(40, '2014-07-20', '2014-07-01'),
-(41, '2014-07-20', '2014-07-01'),
-(42, '2014-07-20', '2014-07-01'),
-(43, '2014-07-20', '2014-07-01'),
-(44, '2014-07-20', '2014-07-01'),
-(45, '2014-07-20', '2014-07-01'),
-(46, '2014-07-20', '2014-07-01'),
-(47, '2014-07-20', '2014-07-01');
 
 -- --------------------------------------------------------
 
