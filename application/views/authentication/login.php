000e6ca1261e519a142d48766afd1c2390bb4a78
Siddharth Kannan <kannan.siddharth12@gmail.com>
2014-08-03 17:40:48 +0530

    Changed the path of all the javascript files of bootstrap to a file inside the project rather than referring to a CDN.

--- application/views/authentication/login.php
+++ application/views/authentication/login.php
@@ -3,7 +3,7 @@
 <head>
 
 	<link rel="stylesheet" href="<?php echo base_url().'bootstrap/bootstrap.min.css'; ?>">
-	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<script src="<?php echo base_url().'bootstrap/bootstrap.min.js'; ?>"></script>
 
 </head>
 
