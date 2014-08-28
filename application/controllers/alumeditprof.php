5b575300862ec1a64437f67d7946cba7f5a1ce60
Siddharth Kannan <kannan.siddharth12@gmail.com>
2014-08-28 22:47:15 +0530

    Added the controller and the view for editing the profile of the alumni. The model will be same as the profiel fetch model.

---
+++ application/controllers/alumeditprof.php
@@ -0,0 +1,76 @@
+<?php
+
+class alumeditprof extends CI_Controller{
+
+	public $cid;
+
+	public function __construct(){
+
+		parent::__construct();
+
+		$this->load->helper('url');
+
+		$this->load->library('session');
+
+		$this->load->model('profilefetchmodel');
+
+		$this->load->helper('form');
+
+		$this->load->library('form_validation');
+
+		$this->cid = 0;
+
+	}
+
+	public function index(){
+
+		echo $this->load->view('templates/header.html', array(), TRUE);
+
+		echo '<h2>You need to specify an Alumni ID.<br/></h2>';
+
+		echo $this->load->view('templates/footer.html', array(), TRUE);
+
+
+	}
+
+	public function editnow($alumid){
+
+		if($this->profilefetchmodel->checkallotment($alumid)){
+
+			echo $this->load->view('templates/header.html', array(), TRUE);
+
+			echo '<h2>This Alumni has not been allotted to you.<br/><h2>';
+
+			$url = site_url('sponscont/');
+
+			echo "<h4><a href='$url'>Go back to Summary Page</a><h4>";
+
+			echo $this->load->view('templates/footer.html', array(), TRUE);
+
+			die;
+		}
+
+		$this->session->set_userdata('temp_aid', $alumid); // additional userdata that will be temporarily
+															// for this function. once used in the model, it will 
+															// unset.
+
+		$data = $this->profilefetchmodel->getDataForEdit($alumid);
+
+		$final_data['data'] = $data;
+
+		$this->load->view('templates/header.html');
+		$this->load->view('profileview/alumprofedit.php', $final_data);
+		$this->load->view('templates/footer.html');
+
+	}
+
+	public function editprofile(){
+
+		return;
+
+	}
+
+}
+
+
+?>
---
+++ application/views/profileview/alumprofedit.php
@@ -0,0 +1,81 @@
+<html>
+
+<head>
+
+	<link rel="stylesheet" href="<?php echo base_url().'bootstrap/bootstrap.min.css'; ?>">
+	<script src="<?php echo base_url().'bootstrap/bootstrap.min.js'; ?>"></script>
+
+</head>
+
+<body style="margin-left: 20px; margin-right: 20px;">
+
+	<?php 
+
+	$ent = $data['fieldVal'];
+
+	$ent_field = $data['fieldData'];
+
+	// echo '<br/><br/>';
+
+	// var_dump($data);
+
+	// echo '<br/><br/>';
+
+	$ent = array_values($ent);
+
+	?>
+
+	<h1 style="text-align: center; color: #999999"> Alumni Profile (Alumni ID <?php echo $ent[0]; ?>)</h1>
+
+	<?php
+
+	$name_fields = array('Alumni ID',
+		'Name',
+		'Department',
+		'Hall',
+		'Alumni Since',
+		'Date of birth',
+		'Roll Number',
+		'Company',
+		'Designation',
+		'Office Address',
+		'Residential Address',
+		'Phone (Office)',
+		'Phone (Residence)',
+		'Email ID (Office)',
+		'Email ID (Personal)'
+		);
+
+		?>
+
+		<table class="table table-bordered table-striped">
+
+			<tbody>
+
+				<?php for($i = 0; $i < count($name_fields); $i = $i + 1): ?>
+
+				<tr>
+
+					<td>
+
+						<?php echo $name_fields[$i]; ?>
+
+					</td>
+
+					<td>
+
+						<?php echo $ent[$i]; ?>
+
+					</td>
+
+				</tr>
+
+			<?php endfor; ?>
+
+		</tbody>
+
+	</table>
+
+</body>
+
+</html>
