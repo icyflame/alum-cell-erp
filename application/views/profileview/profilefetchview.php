ae8ce0d97a2253b201a1fc13b4848a863e012bec
Siddharth Kannan <kannan.siddharth12@gmail.com>
2014-09-07 20:39:12 +0530

    Completed sending the calling requests using AJAX. The request is completed using the get function in the jquery library.

--- application/views/profileview/profilefetchview.php
+++ application/views/profileview/profilefetchview.php
@@ -2,14 +2,20 @@
 
 <head>
 
+	<script src="<?php echo base_url().'bootstrap/jquery-1.10.2.js'; ?>"></script>
+
 	<link rel="stylesheet" href="<?php echo base_url().'bootstrap/bootstrap.min.css'; ?>">
 	<script src="<?php echo base_url().'bootstrap/bootstrap.min.js'; ?>"></script>
+
+	<link rel="stylesheet" href="<?php echo base_url().'font-awesome/font-awesome-3.2.1/css/font-awesome.min.css'; ?>">
+	<link rel="stylesheet" href="<?php echo base_url().'font-awesome/font-awesome-4.1.0/css/font-awesome.min.css'; ?>">
+
 
 </head>
 
 <body style="margin-left: 20px; margin-right: 20px;">
 
-	<h3> Alumni ID <?php echo $alumid ?> </h3>
+	<h3> Alumni ID <?php echo $alumid ?> <i id="doing" class="fa fa-spinner"></i> </h3> 
 	<h2> <?php echo $name ?> </h2>
 	<h4> <?php echo $hall ?> </h4>
 	<h4> <?php echo $dept ?> </h4>
@@ -28,15 +34,79 @@
 
 	<h2> Change the calling status </h2>
 
-	<ul class="nav nav-pills nav-justified" role="menu">
+	<ul id="callingTabs" class="nav nav-pills nav-justified" role="menu">
 
-		<li class="<?php echo $c_1 ?>"><a href="<?php echo site_url('profilefetch/updateCalling/'.$alumid.'/1') ?>">Called (2 way)</a></li>
-		<li class="<?php echo $c_2 ?>"><a href="<?php echo site_url('profilefetch/updateCalling/'.$alumid.'/2') ?>">Negative</a></li>
-		<li class="<?php echo $c_3 ?>"><a href="<?php echo site_url('profilefetch/updateCalling/'.$alumid.'/3') ?>">Neutral</a></li>
-		<li class="<?php echo $c_4 ?>"><a href="<?php echo site_url('profilefetch/updateCalling/'.$alumid.'/4') ?>">Positive</a></li>
+		<li id="calling1" class="<?php echo $c_1 ?>"><a href="#">Called (2 way)</a></li>
+		<li id="calling2" class="<?php echo $c_2 ?>"><a href="#">Negative</a></li>
+		<li id="calling3" class="<?php echo $c_3 ?>"><a href="#">Neutral</a></li>
+		<li id="calling4" class="<?php echo $c_4 ?>"><a href="#">Positive</a></li>
 
 	</ul>
 
 </body>
+
+<script>
+
+$(document).ready(function(){
+
+	$("#calling1").on("click", function(){
+
+		$("#doing").addClass("fa-spin");
+
+		$.get("<?php echo site_url('profilefetch/updateCalling/'.$alumid.'/1') ?>", function(){
+			alert("Calling status successfully updated.");
+		});
+
+		$("#callingTabs .active").removeClass("active");
+		$(this).addClass("active");
+		$("#doing").removeClass("fa-spin");
+
+	});
+
+	$("#calling2").on("click", function(){
+
+		$("#doing").addClass("fa-spin");
+
+		$.get("<?php echo site_url('profilefetch/updateCalling/'.$alumid.'/2') ?>", function(){
+			alert("Calling status successfully updated.");
+		});
+
+		$("#callingTabs .active").removeClass("active");
+		$(this).addClass("active");
+		$("#doing").removeClass("fa-spin");
+
+	});
+
+	$("#calling3").on("click", function(){
+
+		$("#doing").addClass("fa-spin");
+
+		$.get("<?php echo site_url('profilefetch/updateCalling/'.$alumid.'/3') ?>", function(){
+			alert("Calling status successfully updated.");
+		});
+
+		$("#callingTabs .active").removeClass("active");
+		$(this).addClass("active");
+		$("#doing").removeClass("fa-spin");
+
+	});
+
+	$("#calling4").on("click", function(){
+
+		$("#doing").addClass("fa-spin");
+
+		$.get("<?php echo site_url('profilefetch/updateCalling/'.$alumid.'/3') ?>", function(){
+			alert("Calling status successfully updated.");
+		});
+
+		$("#callingTabs .active").removeClass("active");
+		$(this).addClass("active");
+		$("#doing").removeClass("fa-spin");
+
+	});
+
+});
+
+</script>
 
 </html>
